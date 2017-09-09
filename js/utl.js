2436d2c538de63c30672c3910bcade0412a866e8
danilogila <danilosantana58@gmail.com>
2017-09-09 18:59:55 -0300

    Alteração no JS para evitar um botão de dar o submit padrão

--- js/utl.js
+++ js/utl.js
@@ -7,3 +7,15 @@ $('nav a').click(function(e){
 		scrollTop: targetOffset - menuHeight
 	}, 500);
 });
+
+// ---------------------------------------------------------- //
+
+
+// Esse treco evita o botão em cima da section "Fale comigo" de fazer o submit padrão
+
+document.getElementById("teste").addEventListener("click", function(event){
+	event.preventDefault();
+
+});
+
+// ---------------------------------------------------------- //
